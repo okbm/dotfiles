eb444288c2c5b806ddb0d2280a04c4079aad9bdd
okbm <masayoshiokabe.321@gmail.com>
2021-11-24 15:48:08 +0900

    fix fzf path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/masayoshi_o/.vim/dein/repos/github.com/junegunn/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/masayoshi_o/.vim/dein/repos/github.com/junegunn/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.vim/dein/repos/github.com/junegunn/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/$HOME/.vim/dein/repos/github.com/junegunn/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/masayoshi_o/.vim/dein/repos/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.vim/dein/repos/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/masayoshi_o/.vim/dein/repos/github.com/junegunn/fzf/shell/key-bindings.zsh"
+source "$HOME/.vim/dein/repos/github.com/junegunn/fzf/shell/key-bindings.zsh"
